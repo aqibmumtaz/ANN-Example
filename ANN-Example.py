2d0785805fcb66a3bfa688eadb3d1c260293bc2d
Aqib Mumtaz <aqibmumtaz@Aqibs-MacBook-Pro-3.local>
2017-08-16 13:20:19 +0500

    updates

--- ANN-Example.py
+++ ANN-Example.py
@@ -3,77 +3,77 @@
 import numpy as np
 
 
-# The following is a function definition of the sigmoid function, which is the type of non-linearity chosen for this neural net. It is not the only type of non-linearity that can be chosen, but is has nice analytical features and is easy to teach with. In practice, large-scale deep learning systems use piecewise-linear functions because they are much less expensive to evaluate.
+# This code is a definition of the sigmoid function, which is the type of non-linearity chosen for this neural net. It is not the only type of non-linearity that can be chosen, but is has nice analytical features and is easy to teach with.
 
-def nonlin(x, deriv=False):  # Note: there is a typo on this line in the video
+def nonlin(x, deriv=False):
     if(deriv==True):
         return (x*(1-x))
     
-    return 1/(1+np.exp(-x))  # Note: there is a typo on this line in the video
+    return 1/(1+np.exp(-x))
 
 
-# The following code creates the input matrix.
+# The following code creates the input matrix, our network consists of two input nodes and one output node
 
-X = np.array([[0,0],  # Note: there is a typo on this line in the video
+X = np.array([[0,0],
             [0,1],
             [1,0],
             [1,1]])
 
-# The output of the exclusive OR function follows. 
+# The output of the exclusive OR function as follows.
 
-#output data
 y = np.array([[0],
              [1],
              [1],
              [0]])
 
 
-# The seed for the random generator is set so that it will return the same random numbers each time, which is sometimes useful for debugging.
-
+# The seed for the random generator to return the same random numbers each time for being deterministic, which is very useful for debugging.
 np.random.seed(1)
 
 
-# Now we intialize the weights to random values. syn0 are the weights between the input layer and the hidden layer.
+# Initialization of weights to random numbers. syn0 is weight matrix between input layer and first hidden layer.
 
-#synapses
-l1MatrixW = 2
-l1MatrixH = 3
-syn0 = 2*np.random.random((l1MatrixW,l1MatrixH)) - 1
-
-l2MatrixW = l1MatrixH
-l2MatrixH = 1
-syn1 = 2*np.random.random((l2MatrixW,l2MatrixH)) - 1
-
-print ("======= Neural Network with 1 hidden Layer =======")
-print ("\n==== Input Layer nodes ", l1MatrixW, ", Hidden Layer nodes ", l1MatrixH, ", Output Layer nodes ", l2MatrixH, " ====\n")
+# Synapses
+l1Nodes = 2
+l2Nodes = 3
+syn0 = 2*np.random.random((l1Nodes,l2Nodes)) - 1
 
 
-# This is the main training loop. The output shows the evolution of the error between the model and desired. The error steadily decreases. 
+l3Nodes = 1
+syn1 = 2*np.random.random((l2Nodes,l3Nodes)) - 1
+
+print("======= Neural Network with 1 hidden Layer =======")
+print("\n==== Input Layer nodes ", l1Nodes, ", Hidden Layer nodes ", l2Nodes, ", Output Layer nodes ", l3Nodes, " ====\n")
+
+
+# This is iteration training loop for network training. error decreases on each cycle of training by the slop of sigmoid function using gradient descent and back propagation.
 iterations = 100000
 for j in range(iterations):
     
-    # Calculate forward through the network.
+    # Calculating forward through out the network
     l0 = X
     l1 = nonlin(np.dot(l0, syn0))
     l2 = nonlin(np.dot(l1, syn1))
-    
-    # Back propagation of errors using the chain rule. 
+
+    # Calculating error
     l2_error = y - l2
     if(j % 10000) == 0:   # Only print the error every 10000 steps, to save time and limit the amount of output. 
         print("Error: " + str(np.mean(np.abs(l2_error))))
         
+    # Back propagation of errors using the chain rule.
     l2_delta = l2_error*nonlin(l2, deriv=True)
     
     l1_error = l2_delta.dot(syn1.T)
     
     l1_delta = l1_error * nonlin(l1,deriv=True)
     
-    #update weights (no learning rate term)
+    # Updating weights (no alpha learning term here..)
+    # Default Weight assignment equation : W = W + alpha.input.error
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
     
-print ("\nOutput after training ", iterations, " iterations")
-print (l2)
+print("\nOutput after training ", iterations, " iterations")
+print(l2)
     
     
 
